d8e9d538b5cfa75393d3fabc2614260db160e600
aptivator <aptivator@gmail.com>
2017-05-05 16:23:54 -0400

    Update server

--- server.js
+++ server.js
@@ -3,9 +3,5 @@ let app = express();
 let path = require('path');
 let staticPath = path.resolve(__dirname, 'dist');
 
-
 app.use(express.static(staticPath));
-
-console.log(process.env.PORT);
-
 app.listen(process.env.PORT || 12345);
